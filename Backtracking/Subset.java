f59c08ef5226e009ffe9e0626297948f2c0d8260
Satyam <worksatyam62@gmail.com>
2026-03-03 00:26:09 +0530

    Subset

--- Backtracking/Subset.java
+++ Backtracking/Subset.java
@@ -1,5 +1,22 @@
-package Backtracking;
-
-public class Subset {
+class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int arr[]) {
+        // code here
+        ArrayList<Integer>result=new ArrayList<>();
+        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+        
+        helper(0,arr,result,ans);
+        return ans;
+    }
     
+    public static void helper(int start, int []arr,ArrayList<Integer>result,ArrayList<ArrayList<Integer>>ans)
+    {
+        ans.add(new ArrayList<>(result));
+        
+        for(int i=start;i<arr.length;i++)
+        {
+            result.add(arr[i]);
+            helper(i+1,arr,result,ans);
+            result.remove(result.size()-1);
+        }
+    }
 }
